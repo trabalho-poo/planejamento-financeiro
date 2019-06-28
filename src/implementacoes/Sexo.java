1234cc5a9b7b60ec6975983b95041209cd279ac4
Guilherme Mattos <45467253+guilhermemattos16@users.noreply.github.com>
2019-06-28 09:20:11 -0300

    Criação do javaDoc

--- src/implementacoes/Sexo.java
+++ src/implementacoes/Sexo.java
@@ -1,5 +1,13 @@
 package implementacoes;
 
+/**
+ * Classe para realizar a enumeracao do tipo sexo do usuario
+ * para auxiliar na implementacao do programa.
+ * 
+ * @author Grupo (Cleisson diLauro, Franco Flores, Guilherme Mattos, Luciano
+ *         Alves, Natalia Lopes)
+ * @version 1.0 (junho-2019)
+ */
 public enum Sexo {
 	MASCULINO, FEMININO;
 }
--- src/implementacoes/TipoDespesa.java
+++ src/implementacoes/TipoDespesa.java
@@ -1,5 +1,13 @@
 package implementacoes;
 
+/**
+ * Classe para realizar a enumeracao dos tipos de despesas
+ * para auxiliar na implementacao do programa.
+ * 
+ * @author Grupo (Cleisson diLauro, Franco Flores, Guilherme Mattos, Luciano
+ *         Alves, Natalia Lopes)
+ * @version 1.0 (junho-2019)
+ */
 public enum TipoDespesa {
 	ALUGUEL, TELEFONE, INTERNET, ACADEMIA, CLUBE, SUPERMERCADO, LUZ, AGUA;
 }
