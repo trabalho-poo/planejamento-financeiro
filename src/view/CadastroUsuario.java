11e9dad90a4cf3d99721ef1ea756cc98a1d2b12a
cleissondilauro <cleissondilauro@gmail.com>
2019-06-28 18:26:22 -0300

    Telas

--- src/view/CadastroUsuario.java
+++ src/view/CadastroUsuario.java
@@ -8,16 +8,41 @@ import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import implementacoes.Sexo;
-import com.toedter.calendar.JCalendarBeanInfo;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 
-public class CadastroUsuario {
+import javax.swing.JSeparator;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JFormattedTextField;
 
+import javax.swing.JPasswordField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import javax.swing.JButton;
+
+
+public class CadastroUsuario extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private JTextField textField;
+	private JTextField textDataN;
+	private JTextField textField_2;
+	private JPasswordField passwordField;
+	
+
 	/**
 	 * @wbp.nonvisual location=327,131
 	 */
-	private final JCalendarBeanInfo calendarBeanInfo = new JCalendarBeanInfo();
+//	private final JCalendarBeanInfo calendarBeanInfo = new JCalendarBeanInfo();
 
 	/**
 	 * Launch the application.
@@ -47,30 +72,140 @@ public class CadastroUsuario {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 850, 599);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(6, 52, 61, 16);
-		frame.getContentPane().add(lblNome);
-		
-		textField = new JTextField();
-		textField.setBounds(6, 80, 152, 26);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Sexo:");
-		lblNewLabel.setBounds(170, 52, 61, 16);
-		frame.getContentPane().add(lblNewLabel);
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		panel.setForeground(Color.WHITE);
+		panel.setBounds(0, 0, 832, 552);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(634, 267, 140, 27);
+		panel.add(comboBox);
 		comboBox.setModel(new DefaultComboBoxModel(Sexo.values()));
-		comboBox.setBounds(170, 81, 130, 27);
-		frame.getContentPane().add(comboBox);
 		
-		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento:");
-		lblDataDeNascimento.setBounds(306, 52, 138, 16);
-		frame.getContentPane().add(lblDataDeNascimento);
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(CadastroUsuario.class.getResource("/img/essa - Copia.jpg")));
+		label.setBounds(0, 0, 280, 555);
+		panel.add(label);
+		
+		textField = new JTextField();
+		textField.setBounds(313, 184, 286, 22);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(313, 204, 251, 2);
+		panel.add(separator);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblNome.setBounds(313, 155, 56, 16);
+		panel.add(lblNome);
+		
+		JLabel lblCadastroDeUsurio = new JLabel("CADASTRO DE USU\u00C1RIO");
+		lblCadastroDeUsurio.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCadastroDeUsurio.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblCadastroDeUsurio.setEnabled(false);
+		lblCadastroDeUsurio.setBounds(381, 13, 317, 75);
+		panel.add(lblCadastroDeUsurio);
+		
+		textDataN = new JTextField();
+		textDataN.setBounds(634, 182, 140, 22);
+		panel.add(textDataN);
+		textDataN.setColumns(10);
+		
+		JLabel lblDataNascimento = new JLabel("Data Nascimento:");
+		lblDataNascimento.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblDataNascimento.setBounds(634, 155, 140, 16);
+		panel.add(lblDataNascimento);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(634, 202, 140, 2);
+		panel.add(separator_1);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblCpf.setBounds(313, 237, 73, 16);
+		panel.add(lblCpf);
+		
+		JFormattedTextField formattedTextCPF = new JFormattedTextField();
+		formattedTextCPF.setBounds(313, 269, 155, 22);
+		panel.add(formattedTextCPF);
+		
+		JLabel lblRg = new JLabel("RG:");
+		lblRg.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblRg.setBounds(480, 237, 73, 16);
+		panel.add(lblRg);
+		
+		JFormattedTextField formattedTextRG = new JFormattedTextField();
+		formattedTextRG.setBounds(480, 269, 119, 22);
+		panel.add(formattedTextRG);
+		
+		JLabel lblSexo = new JLabel("Sexo:");
+		lblSexo.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblSexo.setBounds(634, 238, 73, 16);
+		panel.add(lblSexo);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(313, 420, 286, 22);
+		panel.add(textField_2);
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblSenha.setBounds(634, 391, 140, 16);
+		panel.add(lblSenha);
+		
+		JLabel lblEmai = new JLabel("E-mai:");
+		lblEmai.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblEmai.setBounds(313, 391, 56, 16);
+		panel.add(lblEmai);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(634, 420, 140, 22);
+		panel.add(passwordField);
+		
+		Button button = new Button("Cadastrar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Dialog", Font.PLAIN, 18));
+		button.setBackground(new Color(47, 79, 79));
+		button.setBounds(472, 490, 177, 33);
+		panel.add(button);
+		
+		JLabel lblDadosPessoais = new JLabel("Dados Pessoais");
+		lblDadosPessoais.setBackground(Color.WHITE);
+		lblDadosPessoais.setForeground(Color.DARK_GRAY);
+		lblDadosPessoais.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblDadosPessoais.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDadosPessoais.setBounds(351, 126, 384, 16);
+		panel.add(lblDadosPessoais);
+		
+		JLabel lblInformaoDaConta = new JLabel("Informa\u00E7\u00E3o da Conta");
+		lblInformaoDaConta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInformaoDaConta.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblInformaoDaConta.setBounds(351, 362, 384, 16);
+		panel.add(lblInformaoDaConta);
+		
+		JButton btnVoltar = new JButton("voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				setVisible(false);
+				new Login().setVisible(true); 
+				
+			}
+		});
+		btnVoltar.setBounds(710, 24, 97, 25);
+		panel.add(btnVoltar);
 	}
 }
--- src/view/Home.java
+++ src/view/Home.java
@@ -3,9 +3,20 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JSplitPane;
+import javax.swing.JScrollPane;
+import java.awt.Color;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Button;
+import java.awt.Font;
 
 public class Home extends JFrame {
 
@@ -32,11 +43,76 @@ public class Home extends JFrame {
 	 */
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 850, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, 188, 553);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		Button button_3 = new Button("Sair");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_3.setForeground(Color.WHITE);
+		button_3.setFont(new Font("Dialog", Font.PLAIN, 15));
+		button_3.setBackground(new Color(47, 70, 79));
+		button_3.setBounds(-16, 458, 222, 40);
+		panel.add(button_3);
+		
+		Button button_1 = new Button("Hist\u00F3rico");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		button_1.setForeground(Color.WHITE);
+		button_1.setFont(new Font("Dialog", Font.PLAIN, 15));
+		button_1.setBackground(new Color(49, 113, 37));
+		button_1.setBounds(-16, 211, 222, 40);
+		panel.add(button_1);
+		
+		Button button_2 = new Button("Relatorios");
+		button_2.setForeground(Color.WHITE);
+		button_2.setFont(new Font("Dialog", Font.PLAIN, 15));
+		button_2.setBackground(new Color(79, 79, 79));
+		button_2.setBounds(-16, 318, 222, 40);
+		panel.add(button_2);
+		
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		Button button = new Button("Nova Movimenta\u00E7\u00E3o");
+		button.setForeground(Color.WHITE);
+		button.setFont(new Font("Dialog", Font.PLAIN, 15));
+		button.setBackground(new Color(47, 79, 79));
+		button.setBounds(-16, 112, 222, 40);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				new NovaMovimentacao().setVisible(true); 
+				
+			}
+		});
+		
+		panel.add(button);
+		
+		JLabel lblFundo = new JLabel("Nova Movimenta\u00E7\u00E3o");
+		lblFundo.setIcon(new ImageIcon(Home.class.getResource("/img/essa - Copia.jpg")));
+		lblFundo.setBounds(0, 0, 188, 553);
+		panel.add(lblFundo);
+		
+		JPanel Home = new JPanel();
+		Home.setBackground(Color.WHITE);
+		Home.setBounds(187, 0, 645, 553);
+		contentPane.add(Home);
 
+	}
 }
